cee0368e3a0b04440fe65e693168ab66f99a9760
Joao Dias <joaoeduardodias123@gmail.com>
2021-06-08 14:27:41 -0300

    refactor: add middleware exception handle errors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import "reflect-metadata";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
+import { AppError } from "./errors/AppError";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 import "./database";
@@ -13,5 +15,20 @@ app.use(express.json());
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+    return response.status(500).json({
+      status: "error",
+      message: `Ìnternal server error - ${err.message}`,
+    });
+    next();
+  }
+);
 
 app.listen(3333, () => console.log("Server is Running!"));
